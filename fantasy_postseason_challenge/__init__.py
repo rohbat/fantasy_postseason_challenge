19bf7730940b14fc1379395e9e22991198cea112
Joe Roddy <jroddy33@gmail.com>
2020-12-31 16:25:23 -0600

    flask_login for login management

--- fantasy_postseason_challenge/__init__.py
+++ fantasy_postseason_challenge/__init__.py
@@ -1,7 +1,9 @@
 import os
 from .db import initialize_db
+from .account import User
 
 from flask import Flask
+from flask_login import LoginManager
 
 def create_app(test_config=None):
     # create and configure the app
@@ -15,6 +17,14 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
 
     initialize_db(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "login"
+
+    @login_manager.user_loader
+    def load_user(username):
+        return User.objects(username=username).first()
 
     with app.app_context():
         from . import routes
