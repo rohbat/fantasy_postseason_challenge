3f20f521edc1b77de336d89c45c9d5f87e44c5c2
Rohan Batra <rohbat@gmail.com>
2020-12-31 16:47:10 -0500

    remove debug prints

--- fantasy_postseason_challenge/__init__.py
+++ fantasy_postseason_challenge/__init__.py
@@ -10,7 +10,6 @@ def create_app(test_config=None):
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py')
-        print(app.config['MONGODB_HOST'])
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -19,5 +18,4 @@ def create_app(test_config=None):
 
     with app.app_context():
         from . import routes
-        print(app.config['MONGODB_HOST'])
         return app
--- fantasy_postseason_challenge/db.py
+++ fantasy_postseason_challenge/db.py
@@ -3,5 +3,4 @@ from flask_mongoengine import MongoEngine
 db = MongoEngine()
 
 def initialize_db(app):
-    print(app.config['MONGODB_HOST'])
     db.init_app(app)
