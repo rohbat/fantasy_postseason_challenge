7c7c7bd826fbd1b9d5d03bf7d0a97ae494971852
Rohan Batra <rohbat@gmail.com>
2020-12-31 02:48:12 -0500

    Change db to mongodb, add app route to check user id for experimentation purpose

--- routes.py
+++ routes.py
@@ -2,10 +2,12 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from flask import current_app as app
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from fantasy_postseason_challenge.db import get_db
-
+mongo = PyMongo(app)
 
 @app.route("/")
 def hello():
@@ -17,20 +19,18 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        db = get_db()
-        e = None
 
+        accounts = mongo.db.test_accounts
+
+        e = None
         if not (username and password):
             e = "username and password required"
-        elif db.execute("SELECT * FROM login WHERE username=?", (username,)).fetchone():
+        elif accounts.find_one({"username" : username}):
             e = f"{username} already exists"
-
+        
         if not e:
-            db.execute(
-                "INSERT INTO login (username, password) VALUES (?, ?)",
-                (username, generate_password_hash(password))
-            )
-            db.commit()
+            new_user = {'username' : username, 'password_hash' : generate_password_hash(password)}
+            post_id = accounts.insert_one(new_user).inserted_id
             return redirect(url_for("login"))
         else:
             flash(e)
@@ -42,22 +42,37 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        db = get_db()
-        e = None
-        user = db.execute("SELECT * FROM login WHERE username=?", (username,)).fetchone()
 
+        accounts = mongo.db.test_accounts
+        account = accounts.find_one({"username" : username})
+        
+        e = None
         if not (username and password):
             e = "username and password required"
-        elif not user:
+        elif not account:
             e = f"{username} does not exist"
-        elif not check_password_hash(user["password"], password):
+        elif not check_password_hash(account["password_hash"], password):
             e = "incorrect password"
 
         if not e:
             session.clear()
-            session["user_id"] = user["user_id"]
+            session["user_id"] = str(account["_id"])
+            flash(str(account["_id"]))
+            time.sleep(10)
             return redirect(url_for("hello"))
         else:
             flash(e)
 
-    return render_template("login.html")
+@app.route("/check_user_id")
+def check_user_id():
+    accounts = mongo.db.test_accounts
+
+    if "user_id" in session:
+        account = accounts.find_one({"_id" : ObjectId(session["user_id"])})
+        
+        if not account:
+            return "No account" + session["user_id"]
+        else:
+            return account['username']
+    else:
+        return "No session"
