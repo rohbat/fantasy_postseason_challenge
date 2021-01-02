e61684aa568e278d18f9229b9169f9b29c32678e
Rohan Batra <rohbat@gmail.com>
2021-01-02 16:44:28 -0500

    Add player display name method

--- fantasy_postseason_challenge/player.py
+++ fantasy_postseason_challenge/player.py
@@ -4,7 +4,10 @@ class Player(db.Document):
     name = db.StringField(required=True)
     team = db.StringField(required=True)
     position = db.StringField(required=True)
-    
+
     week_1_avail = db.BooleanField(default=False)
     week_2_avail = db.BooleanField(default=False)
     week_3_avail = db.BooleanField(default=False)
+
+    def display_name(self):
+        return '[' + team + '] ' + name 
