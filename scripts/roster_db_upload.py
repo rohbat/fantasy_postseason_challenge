2f2a1a87059553b4f5a3fdae8a9b8fc88b1913ac
Rohan Batra <rohbat@gmail.com>
2021-01-04 01:20:17 -0500

    Fix week_1_avail for defenses

--- scripts/roster_db_upload.py
+++ scripts/roster_db_upload.py
@@ -28,6 +28,10 @@ def upload_all_rosters(chrome_driver):
         upload_team_roster(chrome_driver, team_abbrev, url_abbrev_map[team_abbrev], team_name_map[team_abbrev])
 
 def upload_team_roster(chrome_driver, team_abbrev, url_abbrev, team_name):
+    week_1_avail = True
+    if team_abbrev == 'KC' or team_abbrev == 'GB':
+        week_1_avail = False
+
     URL = 'https://www.pro-football-reference.com/teams/' + url_abbrev + '/2020_roster.htm'
     chrome_driver.get(URL)
 
@@ -67,7 +71,7 @@ def upload_team_roster(chrome_driver, team_abbrev, url_abbrev, team_name):
                     team=team_abbrev,
                     position='D/ST',
                     games_started=16,
-                    week_1_avail=True)
+                    week_1_avail=week_1_avail)
     player.display_name = player.get_display_name()
     print(player.name + ' | ' + player.team + ' | ' + player.position + ' | ' + str(player.games_started) + ' | ' + player.display_name)
     player.save()
