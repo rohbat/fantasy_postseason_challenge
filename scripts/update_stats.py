d1f90ed1075a859703c67c77aeffb27d570cb2e6
Joe Roddy <jroddy33@gmail.com>
2021-01-06 22:13:13 -0600

    player stats/score scraper--offense only

--- scripts/update_stats.py
+++ scripts/update_stats.py
@@ -2,9 +2,14 @@ from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 from mongoengine import *
 import shutil
+import configparser
 
-def collect_scores(chrome_driver):
-    game_code = "202101030cle"
+from roster_db_upload import Player, PlayerStats
+
+# TODO: edit with correct games
+GAMES_TO_COLLECT = ["202101030cle"]
+
+def collect_scores(game_code, chrome_driver):
     url = f"https://www.pro-football-reference.com/boxscores/{game_code}.htm"
 
     chrome_driver.get(url)
@@ -15,23 +20,29 @@ def collect_scores(chrome_driver):
     rows = offense_table.find_all("tr")
 
     for row in rows:
-        player = row.a
-        if not player:
+        name = row.a
+        if not name:
             continue
 
-        player = player.text.strip()
+        name = name.text.strip()
         stat_list = ["pass_yds", "pass_td", "rush_yds", "rush_td",
             "rec_yds", "rec_td", "rec", "pass_int", "fumbles"]
-
-        stat_values = {
+        stats = {
             stat: int(row.find('td', {'data-stat': stat}).text)
             for stat in stat_list
         }
+        team = row.find("td", {"data-stat": "team"}).text
 
-        print(player, offense_score(**stat_values))        
+        scores = offense_score(**stats)
+        stats["score_normal"] = scores[0]
+        stats["score_half_ppr"] = scores[1]
+        stats["score_ppr"] = scores[2]
 
+        player_stats = PlayerStats(**stats)
 
-
+        # TODO: hard coded week 1
+        Player.objects(name=name, team=team).update_one(set__week_1_stats=player_stats)
+        # this assumes unique name/team pairs
 
 def offense_score(
     pass_yds=0,
@@ -40,7 +51,6 @@ def offense_score(
     rush_td=0,
     rec_yds=0,
     rec_td=0,
-    # ppr_multiplier=0,
     rec=0,
     pass_int=0,
     fumbles=0,
@@ -53,7 +63,6 @@ def offense_score(
         + (rush_td * 6)
         + (rec_yds * 0.1)
         + (rec_td * 6)
-        # + (rec * ppr_multiplier)
         - (pass_int * 2)
         - (fumbles * 2)
     )
@@ -71,11 +80,19 @@ def kicker_stats():
     pass
 
 if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read('./config.txt')
+    mongo_host = config['DEFAULT']['MONGODB_HOST']
+    connect(host=mongo_host)
+
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     chrome_driver_path = shutil.which("chromedriver")
     chrome_driver = webdriver.Chrome(options=options, executable_path=chrome_driver_path)
 
-    collect_scores(chrome_driver)
+
+    week = 1
+    for game in GAMES_TO_COLLECT:
+        collect_scores(game, chrome_driver)
 
     chrome_driver.close()
