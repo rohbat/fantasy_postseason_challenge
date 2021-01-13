030cb7e3117806a348fc561bbca789140a4f351f
Joe Roddy <jroddy33@gmail.com>
2021-01-12 22:54:58 -0600

    update select_team to week 2

--- fantasy_postseason_challenge/dashboard.py
+++ fantasy_postseason_challenge/dashboard.py
@@ -43,25 +43,26 @@ def logged_in_homepage():
 @login_required
 def select_team(league_id):
     # first game of first week @ Jan 9, 1:05 pm ET -> 6:05 pm UTC
-    if datetime.now(tz.UTC) > datetime(2021, 1, 9, 18, 5, tzinfo=tz.UTC):
-        e = "Picks have locked for this week"
-        flash(e)
-        return redirect(url_for("dashboard.view_league", league_id=league_id))
+    # if datetime.now(tz.UTC) > datetime(2021, 1, 9, 18, 5, tzinfo=tz.UTC):
+    #     e = "Picks have locked for this week"
+    #     flash(e)
+    #     return redirect(url_for("dashboard.view_league", league_id=league_id))
+    week = 2
 
     form = SelectTeamForm(request.form)
     if request.method == "POST":
-        if form.validate_week_1():
+        if getattr(form, f'validate_week_{week}')(): # this is so cool
             league = try_get_league_by_id(league_id)
             if league:
                 for member in league.member_list:
                     if member.account_id == current_user.id:
-                        if not member.week_1_team:
+                        if not getattr(member, f'week_{week}_team', None):
                             fantasy_team = FantasyTeam()
                             fantasy_team.save()
-                            member.week_1_team = fantasy_team
+                            setattr(member, f'week_{week}_team', fantasy_team)
                             league.save()
                         else:
-                            fantasy_team = member.week_1_team
+                            fantasy_team = getattr(member, f'week_{week}_team')
 
                         fantasy_team.QB = ObjectId(form.data["QB"])
                         fantasy_team.RB1 = ObjectId(form.data["RB1"])
@@ -81,12 +82,12 @@ def select_team(league_id):
             e = "Invalid team composition"
             flash(e)
 
-    qbs = sorted(Player.objects(position="QB", week_1_avail=True), key=lambda x: x.games_started, reverse=True)
-    rbs = sorted(Player.objects(position="RB", week_1_avail=True), key=lambda x: x.games_started, reverse=True)
-    wrs = sorted(Player.objects(position="WR", week_1_avail=True), key=lambda x: x.games_started, reverse=True)
-    tes = sorted(Player.objects(position="TE", week_1_avail=True), key=lambda x: x.games_started, reverse=True)
-    ks = sorted(Player.objects(position="K", week_1_avail=True), key=lambda x: x.games_started, reverse=True)
-    d_sts = sorted(Player.objects(position="D/ST", week_1_avail=True), key=lambda x: x.games_started, reverse=True)
+    qbs = sorted(Player.objects(**{'position': 'QB', f'week_{week}_avail': True}), key=lambda x: x.games_started, reverse=True)
+    rbs = sorted(Player.objects(**{'position': 'RB', f'week_{week}_avail': True}), key=lambda x: x.games_started, reverse=True)
+    wrs = sorted(Player.objects(**{'position': 'WR', f'week_{week}_avail': True}), key=lambda x: x.games_started, reverse=True)
+    tes = sorted(Player.objects(**{'position': 'TE', f'week_{week}_avail': True}), key=lambda x: x.games_started, reverse=True)
+    ks = sorted(Player.objects(**{'position': 'K', f'week_{week}_avail': True}), key=lambda x: x.games_started, reverse=True)
+    d_sts = sorted(Player.objects(**{'position': 'D/ST', f'week_{week}_avail': True}), key=lambda x: x.games_started, reverse=True)
 
     form.QB.choices = [(qb.id, qb.display_name) for qb in qbs]
     form.RB1.choices = form.RB2.choices = [(rb.id, rb.display_name) for rb in rbs]
@@ -139,8 +140,8 @@ def view_league(league_id):
 
     data = []
 
+    # TODO: un-hardcode 3
     for week in range(1, 3):
-        print(week)
         league_teams = [getattr(member, f'week_{week}_team', None) for member in league_members]
         week_data = []
         if league_teams:
@@ -157,7 +158,6 @@ def view_league(league_id):
                     score_displayed = default_score_displayed
 
                 for team in league_teams:
-                    print('here')
                     if team:
                         player = getattr(team, pos)
                         name = player.display_name
@@ -169,9 +169,6 @@ def view_league(league_id):
         else:
             week_data = [('set your lineup', 0, *team_colors['None'])] * len(positions)
         data.append(week_data)
-    print()
-    print(data[0])
-    print(data[1])
 
     return render_template(
         "view_league.html",
