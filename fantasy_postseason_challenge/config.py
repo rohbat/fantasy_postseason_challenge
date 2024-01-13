05300e6eff614b87db1fea89b25226b2fff9f0d5
Paulo Nascimento <pnascimento@uchicago.edu>
2024-01-13 11:48:39 -0800

    added config

---
+++ fantasy_postseason_challenge/config.py
@@ -0,0 +1,36 @@
+import os
+from datetime import datetime
+from dateutil import tz
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGODB_HOST = f"mongodb+srv://thepaulonascimento:{os.getenv('MONGO_DB_PW')}@postseasonchallenge.drcj3it.mongodb.net/psc_test?retryWrites=true&w=majority"
+
+PLAYOFF_START_TIMES = {
+    'wildcard': datetime(2024, 1, 13, 13, 5, tzinfo=tz.UTC),
+    'divisional': datetime(2024, 1, 20, 13, 5, tzinfo=tz.UTC),
+    'championship': datetime(2024, 1, 28, 13, 5, tzinfo=tz.UTC),
+}
+
+class Config:
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'default_secret_key')
+    DEBUG = False
+    TESTING = False
+    # Define other global settings
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MONGODB_SETTINGS = {
+        'host': MONGODB_HOST,  # Read from .env file
+        'alias': 'psc_test',
+        # Add other connection settings as needed
+    }
+
+class ProductionConfig(Config):
+    # Production specific configuration
+    MONGODB_SETTINGS = {
+        'host': MONGODB_HOST,
+        'alias': 'psc_prod',
+        # Add other connection settings as needed
+    }
